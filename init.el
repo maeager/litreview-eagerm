72ae4b2756daffb779b74ad5a7313e01e8b724fd
Michael Eager <mick.eager@gmail.com>
2013-02-24 21:42:48 +1100

    Re-organising headers.

--- init.el
+++ init.el
@@ -235,7 +235,7 @@
 	       "\% -*- mode: latex; mode: visual-line; TeX-master: t; TeX-PDF-mode: t -*-
      \\documentclass[11pt,a4paper,twoside,openright]{book}
        \\usepackage{../org-manuscript/style/uomthesis} 
-       \\input{../SimpleResponsesChapter/user-defined}
+       \\input{../org-manuscript/misc/user-defined}
        \\usepackage[nonumberlist,acronym]{glossaries}
        \\input{../org-manuscript/misc/glossary} 
        \\makeglossaries
